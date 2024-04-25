8198be51394955f8591945640386bcd4887e6067
SunniJB <nepimpeta@gmail.com>
2024-04-25 12:45:30 +0200

    Add item

---
+++ Assets/_Scripts/Systems/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public Sprite icon;
+    public AudioClip soundEffect;
+    public GameObject model;
+}
