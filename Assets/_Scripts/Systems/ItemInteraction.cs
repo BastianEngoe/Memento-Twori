7d23abd701b2d06243ef191f947a31ebf82ba526
SunniJB <nepimpeta@gmail.com>
2024-04-25 12:53:03 +0200

    Fixt fixy

---
+++ Assets/_Scripts/Systems/ItemInteraction.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ItemInteraction : MonoBehaviour
+{
+    public UnityEvent onItemInteraction;
+    public GameObject itemPrefab;
+
+    public void compareItem(GameObject itemPrefab)
+    {
+        //Debug.Log("Held item is: " + itemPrefab.name + " and required item is: " + this.itemPrefab.name);
+        if (itemPrefab.name == this.itemPrefab.name + "(Clone)")
+        {
+            onItemInteraction.Invoke();
+            Destroy(GetComponent<ItemInteraction>());
+        }
+    }
+
+    public bool compareItemBool(GameObject itemPrefab)
+    {
+        if (itemPrefab.name == this.itemPrefab.name)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
