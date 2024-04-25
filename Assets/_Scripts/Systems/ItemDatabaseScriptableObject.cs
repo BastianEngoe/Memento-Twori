364a76419824b6d396205190791daa916ca50cdf
SunniJB <nepimpeta@gmail.com>
2024-04-25 12:40:49 +0200

    Trying

--- Assets/_Scripts/Systems/ItemDatabaseScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ItemDatabase", order = 1)]
-public class ItemDatabaseScriptableObject : ScriptableObject
-{
-    public List<Item> ItemDatabase;
-}
